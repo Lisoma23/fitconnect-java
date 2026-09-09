b358d212d854ff44e9f8d3676e0424ff06607652
Sofia <115009629+Lisoma23@users.noreply.github.com>
2026-09-09 12:05:02 +0200

    Update class-service/src/main/java/com/formation/classes/model/FitnessClass.java
    Co-authored-by: Louisan TCHITOULA <96016326+LTOssian@users.noreply.github.com>

--- class-service/src/main/java/com/formation/classes/model/FitnessClass.java
+++ class-service/src/main/java/com/formation/classes/model/FitnessClass.java
@@ -151,8 +151,8 @@ public class FitnessClass {
         return dateTime;
     }
 
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
+    public void setDateTime() {
+        this.dateTime = LocalDateTime.now();
     }
 
     public ClassStatus getStatus() {
